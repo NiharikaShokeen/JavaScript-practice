ca3599f604699ae259db3a404ca9555736cf55b5
Niharika Shokeen <niharika.shokeen05@gmail.com>
2026-04-14 18:35:38 +0530

    generate random colors for boxes using RGB and hexadecimal methods

--- Practice-js/colorBoxes.js
+++ Practice-js/colorBoxes.js
@@ -1,6 +1,6 @@
 
 //generate random colors of text and bg of boxes..
-let boxes = document.querySelector(".container").children;
+let boxes1 = document.querySelector(".container").children;
 
 function getRandomColor(){
     // randomNum(0 to 255)= 0 + random(0to1) * 255 ;
@@ -10,7 +10,30 @@ function getRandomColor(){
 
     return(` rgb(${var1} ${var2} ${var3}) `);
 }
-Array.from(boxes).forEach(e=>{
+Array.from(boxes1).forEach(e=>{
     e.style.backgroundColor = getRandomColor();
     e.style.color = getRandomColor();
 })
+
+
+
+//using hexadecimal
+
+var boxes2 = document.querySelectorAll(".box");
+
+for(var i=0 ; i< boxes2.length; i++){
+    var box = boxes2[i];
+
+    //generate random color (16777215 is white in decimal)
+    var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+    //generate random bg color
+    var randomBg = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+    //asign random color and Bg
+    box.style.color = randomColor;
+    box.style.backgroundColor = randomBg;
+
+    //set content to show color code
+    box.textContent = randomColor;
+}
